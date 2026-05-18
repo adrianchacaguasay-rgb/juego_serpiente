b520ebfc1518b67a5b2157170246ac8f004edaa6
adrianchacaguasay-rgb <adrianchacaguasay@gmail.com>
2026-05-18 04:16:29 -0500

    ejercicio final parte 2

--- serpiente.js
+++ serpiente.js
@@ -3,6 +3,13 @@
 const canvas = document.getElementById("canvasJuego");
 const ctx = canvas.getContext("2d");
 const tamanoCelda = 25;
+const serpiente = [
+  { x: 10, y: 11 },
+  { x: 11, y: 11 },
+  { x: 12, y: 11 },
+  { x: 12, y: 12 },
+];
+
 
 
 
@@ -34,46 +41,33 @@ function dibujarTablero() {
     ctx.stroke();
   }
 }
-  function pintarParte(lineaX, lineaY, color) {
-    let valorx = lineaX * tamanoCelda;
-    let valory = lineaY * tamanoCelda;
-    ctx.fillStyle = color;
-    ctx.fillRect(valorx, valory, tamanoCelda, tamanoCelda);
+function pintarParte(lineaX, lineaY, color) {
+  let valorx = lineaX * tamanoCelda;
+  let valory = lineaY * tamanoCelda;
+  ctx.fillStyle = color;
+  ctx.fillRect(valorx, valory, tamanoCelda, tamanoCelda);
 
-    ctx.strokeStyle = color;
-    ctx.strokeRect(valorx, valory, tamanoCelda, tamanoCelda)
+  ctx.strokeStyle = color;
+  ctx.strokeRect(valorx, valory, tamanoCelda, tamanoCelda)
+}
+
+function pintarSerpiente() {
+  for (let indice = 0; indice < serpiente.length; indice++) {
+    let parte = serpiente[indice];
+    if (parte.x == 12 && parte.y == 12) {
+      pintarParte(parte.x, parte.y, "yellow");
+    } else {
+      pintarParte(parte.x, parte.y, "red");
+    }
   }
+}
 
 
 function dibujarTodo() {
   limpiarCanvas();
   dibujarTablero();
-    pintarParte(5, 5, "red");
-  pintarParte(10, 2, "red");
+  pintarSerpiente();
 
-  pintarParte(
-    (canvas.height - tamanoCelda) / tamanoCelda,
-    (canvas.width - tamanoCelda) / tamanoCelda,
-    "red"
-  );
-
-  pintarParte(
-    (canvas.height - tamanoCelda) / tamanoCelda,
-    10,
-    "red"
-  );
-
-  pintarParte(
-    0,
-    (canvas.width - tamanoCelda) / tamanoCelda,
-    "red"
-  );
-
-  pintarParte(
-    (canvas.height - tamanoCelda) / tamanoCelda,
-    0,
-    "red"
-  );
 }
 
 
